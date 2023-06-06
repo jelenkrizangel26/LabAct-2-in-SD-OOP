afd4fea89da88d323908b7dccf8760d5dc0195ec
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-06-06 15:59:24 +0800

    Done adding the code

---
+++ fibonacciSeries.java
@@ -0,0 +1,9 @@
+public class fibonacciSeries{
+    public static void main(String[] args) {
+        int n = 10;
+        int t1 = 0, t2 = 1;
+
+        System.out.print("First " + n + " terms: ");
+
+    }
+}
