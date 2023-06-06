46dc7c0919f3ac7b1eefa1b3dd655a60937addae
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-06-06 14:16:30 +0800

    Done finding the value and updating the code

--- find_maxValue.java
+++ find_maxValue.java
@@ -2,5 +2,13 @@ public class find_maxValue {
     public static void main(String[] args) {
         int[] numbers = {1, 3, 5, 7, 9, 2, 4, 6, 8};
         int max = numbers[0];
+    
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > max) {
+                max = numbers[i];
+            }
+        }
+
+        System.out.println("Maximum value: " + max);    
     }
 }
