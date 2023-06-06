241cf08f92eb547ebdef3065a937aa27da095f54
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-06-06 14:37:18 +0800

    Done adding the code

---
+++ PrimeNumber.java
@@ -0,0 +1,15 @@
+public class PrimeNumber {
+    public static void main(String[] args) {
+        int num = 29; // the number to test
+        boolean isPrime = true;
+
+
+        if(isPrime) {
+            System.out.prinln(num + "is a prime number.");
+        }
+        else{
+            System.out.prinln(num + "is not a prime number.");
+        }
+    }
+    
+}
