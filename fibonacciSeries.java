eb798b9959c6b344f9beb5534aa7f96bf9964b31
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-06-06 16:22:22 +0800

    Done adding design to the output and not it's done!

--- fibonacciSeries.java
+++ fibonacciSeries.java
@@ -3,7 +3,10 @@ public class fibonacciSeries{
         int n = 10;
         int t1 = 0, t2 = 1;
 
-        System.out.print("First " + n + " terms: ");
+        System.out.println("\u001B[32m****************** Fibonacci Series ******************\u001B[0m");
+        System.out.println();
+        System.out.print(" First " + n + " terms: ");
+        
 
         for (int i = 0; i <= n; ++i) {
             System.out.print(t1 + ", ");
@@ -13,5 +16,7 @@ public class fibonacciSeries{
             t1 = t2;
             t2 = tn;
         }
+        System.out.println();
+        System.out.println("\u001B[32m******************************************************\u001B[0m");
     }
 }
