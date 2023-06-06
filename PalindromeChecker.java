ff9ec92883880eae56ccefc8b2a6907f31c76d7b
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-06-06 16:59:36 +0800

    Code is finalized!

--- PalindromeChecker.java
+++ PalindromeChecker.java
@@ -2,7 +2,23 @@ public class PalindromeChecker {
     public static void main(String[] args) {
         String original = "racecar";
         String reversed = "";
+        
+        System.out.println("\u001B[32m************* Palindrome Checker *************\u001B[0m");
 
+        int origLength = original.length();
 
+        for (int i = (origLength - 1); i >=0; --i) {
+            reversed = reversed + original.charAt(i);
+        }
+
+        if (original.toLowerCase().equals(reversed.toLowerCase())) {
+            System.out.println();
+            System.out.println(original + " is a Palindrome String.");
+        }
+        else {
+            System.out.println();
+            System.out.println(original + " is not a Palindrome String.");
+        }
+        System.out.println("\u001B[32m**********************************************\u001B[0m");
     }
 }
