440a5251f2879674e6d087a4aa483cb46782c2f8
Jelen Kriz Angel Mampusti <92630388+jelenkrizangel26@users.noreply.github.com>
2023-06-06 16:04:29 +0800

    Im done adding the code needed to get the terms but I have an error since it only shows only till 34 and I need till 55

--- fibonacciSeries.java
+++ fibonacciSeries.java
@@ -5,5 +5,13 @@ public class fibonacciSeries{
 
         System.out.print("First " + n + " terms: ");
 
+        for (int i = 1; i <= n; ++i) {
+            System.out.print(t1 + ", ");
+
+            //for next terms
+            int tn = t1 + t2;
+            t1 = t2;
+            t2 = tn;
+        }
     }
 }
